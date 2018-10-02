0eaf429d85e616b1d05a1e391490b5bc7e5af937
Suheyl Turac <skbsturac@live.com>
2018-10-02 22:02:18 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -50,7 +50,7 @@ def until_iterator(number_of_times)
   until number_of_times == 7 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   puts phrase
-  number_of_times
+  number_of_times += 1 
 
 end
 
